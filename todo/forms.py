8e36719ca30e2529626c17c5d190601c7f931d1a
vessoutraore <yvtraore94@yahoo.com>
2024-04-23 19:03:56 +0000

    aws cloud

---
+++ todo/forms.py
@@ -0,0 +1,27 @@
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm 
+
+from django.contrib.auth.models import User 
+
+from django.forms.widgets import PasswordInput, TextInput
+
+from django import forms
+
+
+# - Create a user
+
+class CreateUserForm(UserCreationForm):
+
+    class Meta:
+
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+# - Authentificate a user
+
+class LoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=TextInput())
+
+    password = forms.CharField(widget=PasswordInput())
